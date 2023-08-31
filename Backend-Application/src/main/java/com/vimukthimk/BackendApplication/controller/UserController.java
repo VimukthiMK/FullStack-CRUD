8371c38b952fe9b25581be2d4d8110107fd5a6ee
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-08-31 14:02:04 +0530

    Created UserController

---
+++ Backend-Application/src/main/java/com/vimukthimk/BackendApplication/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.vimukthimk.BackendApplication.controller;
+
+import com.vimukthimk.BackendApplication.model.User;
+import com.vimukthimk.BackendApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
