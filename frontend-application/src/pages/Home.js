0d9a0fe4c7f129c4f90c690fa818bb6a2cccc213
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-08-31 15:51:49 +0530

    Created Home.js

--- frontend-application/src/pages/Home.js
+++ frontend-application/src/pages/Home.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">UserName</th>
+      <th scope="col">name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
     </div>
   )
 }
