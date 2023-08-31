5fd271c4dfbc343020f17792e1757fe153aaed00
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-08-31 17:21:35 +0530

    Home.js-Added UI elements

--- frontend-application/src/pages/Home.js
+++ frontend-application/src/pages/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className='py-4'>
         <table className="table border shadow">
   <thead>
-    <tr>
+    <tr className='text-center align-middle'>
       <th scope="col">ID</th>
       <th scope="col">UserName</th>
       <th scope="col">name</th>
@@ -29,11 +29,16 @@ export default function Home() {
   <tbody>
    {
     users.map((user,index)=>(
-        <tr>
+        <tr className='text-center align-middle'>
             <th scope='row' key={index}>{index+1}</th>
             <td>{user.username}</td>
             <td>{user.name}</td>
             <td>{user.email}</td>
+            <td>
+                <button className='btn btn-secondary mx-2'>View</button>
+                <button className='btn btn-outline-primary mx-2'>Edit</button>
+                <button className='btn btn-danger mx-2'>Delete</button>
+            </td>
         </tr>
     ))
    }
