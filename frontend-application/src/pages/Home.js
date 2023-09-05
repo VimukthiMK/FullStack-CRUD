5dcc0b375a4d123c774e0588308d1be32968d625
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-05 19:21:13 +0530

    Fixed-react-dom

--- frontend-application/src/pages/Home.js
+++ frontend-application/src/pages/Home.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-    const {id}=useParams()
-
     const [users,setUsers]=useState([])
 
         useEffect(()=> {
@@ -14,12 +12,7 @@ export default function Home() {
         const loadUsers=async()=> {
             const result=await axios.get("http://localhost:8080/api/users")
             setUsers(result.data)
-        }  
-        
-      const deleteUser=async(id)=>{
-        await axios.delete(`http://localhost:8080/api/user/${id}`)
-        loadUsers()
-      }
+        }   
   
     return (
     <div className='container'>
@@ -43,9 +36,9 @@ export default function Home() {
             <td>{user.name}</td>
             <td>{user.email}</td>
             <td>
-            <Link className='btn btn-secondary mx-2' to={`/viewuser/${user.id}`}> View </Link>
+            <Link className='btn btn-secondary mx-2' to={"/viewuser"}> View </Link>
             <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}> Edit </Link>
-            <button className='btn btn-danger mx-2' onClick={()=>deleteUser(user.id)}> Delete </button>
+            <Link className='btn btn-danger mx-2' to={"/deleteuser"}> Delete </Link>
             </td>
         </tr>
     ))
