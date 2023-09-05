6e506eb7f9758e54e00ba94861340ca378f22a3b
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-05 20:01:37 +0530

    finished viewuser.js

--- frontend-application/src/users/ViewUser.js
+++ frontend-application/src/users/ViewUser.js
@@ -1,7 +1,29 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link , useParams } from 'react-router-dom'
+import axios from 'axios'
 
 export default function ViewUser() {
+
+    const {id}=useParams()
+    
+  const [user, setUser] = useState({
+    name: '',
+    username: '',
+    email: '',
+  })
+
+    useEffect(()=> {
+      loadUser() 
+    },[])
+
+    const loadUser = async () => {
+      try {
+        const result = await axios.get(`http://localhost:8080/api/user/${id}`);
+        setUser(result.data);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    };
   return (
     <div className='container'>
         <div className='row'>
@@ -14,12 +36,15 @@ export default function ViewUser() {
                   <ul className='list-group list-group-flush'>
                     <li className='list-group-item'>
                       <b>Name:</b>
+                      {user.name}
                     </li>
                     <li className='list-group-item'>
                       <b>UserName:</b>
+                      {user.username}
                     </li>
                     <li className='list-group-item'>
                       <b>Email:</b>
+                      {user.email}
                     </li>
                   </ul>
                 </div>
