36c750b0728910007ed405c407126a98dbf64a12
vimukthikulathilaka@gmail.com <vimukthikulathilaka@gmail.com>
2023-09-05 19:38:47 +0530

    Created viewuser.js

--- frontend-application/src/users/ViewUser.js
+++ frontend-application/src/users/ViewUser.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 export default function ViewUser() {
   return (
-    <div>
-      <p>View User</p>
-    </div>
+    <div className='container'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow '>
+            <h2 className='text-center m-4'>View User</h2>
+
+              <div className='card'>
+                <div className='card-header'>
+                  Details of User ID:
+                  <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'>
+                      <b>Name:</b>
+                    </li>
+                    <li className='list-group-item'>
+                      <b>UserName:</b>
+                    </li>
+                    <li className='list-group-item'>
+                      <b>Email:</b>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <Link className='btn btn-primary mx-2' to={"/"}> Back to Home </Link>
+            
+          </div>
+  
+        </div>
+        
+      
+      </div>
   )
 }
